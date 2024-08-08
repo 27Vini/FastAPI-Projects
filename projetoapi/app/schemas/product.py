976bdb00b29749e1fb243634b704c4e18e5d33f5
Vini <vouverney27@gmail.com>
2024-08-08 13:14:25 -0300

    Product schema, model, add

---
+++ projetoapi/app/schemas/product.py
@@ -0,0 +1,26 @@
+from app.schemas.base import CustomBaseModel
+from pydantic import field_validator
+import re
+
+class Product(CustomBaseModel):
+    name : str
+    slug : str
+    price : float
+    stock : int
+
+    @field_validator('slug')
+    def validate_slug(cls,value):
+        if not re.match('^([a-z]|-|_)+$',value):
+            raise ValueError('Invalid Slug')
+        return value
+    
+    @field_validator('price')
+    def validate_price(cls,value):
+        if value <= 0:
+            raise ValueError("Invalid price")
+        return value
+
+
+class ProductInput(CustomBaseModel):
+    category_slug : str
+    product : Product
