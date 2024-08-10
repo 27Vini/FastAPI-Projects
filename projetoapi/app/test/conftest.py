845a76259de48b31a2d3a4135f86b20e98d7834e
Vini <vouverney27@gmail.com>
2024-08-10 13:23:06 -0300

    Product update

--- projetoapi/app/test/conftest.py
+++ projetoapi/app/test/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from app.db.connection import Session
 from app.db.models import Category as CategoryModel
-
+from app.db.models import Product as ProductModel
 
 
 @pytest.fixture()
@@ -33,4 +33,26 @@ def categories_on_db(db_session):
 
     for category in categories:
         db_session.delete(category)
+    db_session.commit()
+
+@pytest.fixture()
+def product_on_db(db_session):
+    category = CategoryModel(name='Carro',slug='carro')
+    db_session.add(category)
+    db_session.commit()
+    product = ProductModel(
+        name='Camisa Abidas',
+        slug='camisa-abidas',
+        price=100.99,
+        stock=20,
+        category_id=category.id
+    )
+    db_session.add(product)
+    db_session.commit()
+
+    yield product
+
+    db_session.refresh(product)
+    db_session.delete(product)
+    db_session.delete(category)
     db_session.commit()
