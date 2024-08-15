65f6214772628784b35de5c14c9accd8d87809f8
Vini <vouverney27@gmail.com>
2024-08-15 10:47:53 -0300

    Created User and add user

--- projetoapi/app/main.py
+++ projetoapi/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes.category_routes import router as category_router
 from app.routes.product_routes import router as product_router
+from app.routes.user_routes import router as user_routes
 app = FastAPI()
 
 @app.get('/health-check')
@@ -9,3 +10,4 @@ def health_check():
 
 app.include_router(category_router)
 app.include_router(product_router)
+app.include_router(user_routes)
